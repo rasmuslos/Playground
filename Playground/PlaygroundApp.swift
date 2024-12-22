6556f61d59bf1870bf5b0e921f816e087af83f7a
Rasmus Krämer <git@rfk.io>
2024-12-22 11:23:43 +0100

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,32 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Rasmus Krämer on 22.12.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PlaygroundApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
